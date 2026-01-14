4a8bda03693d00a441f632f07690bd1f92d6a508
vendaJackson <mulwelifx45@gmail.com>
2026-01-14 09:03:50 +0200

    API initialised

---
+++ src/main/java/com/MulweliCoding/FraudSignalEngine/FraudSignalEngineApplication.java
@@ -0,0 +1,13 @@
+package com.MulweliCoding.FraudSignalEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudSignalEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudSignalEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MulweliCoding/FraudSignalEngine/FraudSignalEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MulweliCoding.FraudSignalEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudSignalEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
